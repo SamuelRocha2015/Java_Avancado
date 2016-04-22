673e81a1e2432b5e242e91473664109b453269a1
Samuel Rocha <samuelrodriguessr@hotmail.com>
2016-04-22 16:38:56 -0300

    Aula 01: Alterações no Projeto -Classes Filme e Categoria

--- src/main/java/br/com/codeshouse/locadora/modelo/Filme.java
+++ src/main/java/br/com/codeshouse/locadora/modelo/Filme.java
@@ -1,14 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.com.codeshouse.locadora.modelo;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+/**
+ *
+ * @author Samuel
+ */
+
+@Entity
+@Table(name = "filme")
 public class Filme implements Serializable {
-    
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_filme")
     private Integer id;
     private String titulo;
     private String sinopse;
+    @ManyToOne (optional = false)
+    @JoinColumn(name = "id_categoria")
     private Categoria categoria;
 
     public Integer getId() {
@@ -42,5 +66,5 @@ public class Filme implements Serializable {
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
-    
+
 }
